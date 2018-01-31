38b5a5fe73c2e7392939ab020fea311e2460f4a9
Matthias Raab <M.Raab@intershop.de>
2018-01-31 09:19:43 +0100

    adapt license header

--- src/main/java/com/intershop/gradle/resourcelist/CartridgeResourceListPlugin.java
+++ src/main/java/com/intershop/gradle/resourcelist/CartridgeResourceListPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Intershop Communications AG.
+ * Copyright 2018s Intershop Communications AG.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
